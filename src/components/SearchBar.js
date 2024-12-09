e49d17aacb7826ced9286e0af027937ad3904da1
Martin Emil Brabenec <martinemilbrabenec@gmail.com>
2024-12-09 12:44:59 +0100

    improved search bar, moved the search icon to the search recipe input

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -13,27 +13,26 @@ export default function SearchBar({ onSearch }) {
 
   return (
     <form onSubmit={handleSubmit} className="form-control w-full max-w-2xl mx-auto mb-8">
-      <div className="input-group">
-        <div className="relative flex-grow">
-            
+      <div className="join w-full">
+        <div className="relative w-full">
           <input 
             type="text" 
             value={query}
             onChange={(e) => setQuery(e.target.value)}
             placeholder="Search recipes..." 
-            className="input input-bordered w-full pl-10"
+            className="input input-bordered join-item w-full pl-10" 
           />
           <svg 
             xmlns="http://www.w3.org/2000/svg" 
-            className="h-5 w-5 absolute left-3 top-1/2 -translate-y-1/2 text-gray-400"
-            fill="none"
-            viewBox="0 0 24 24"
+            className="h-5 w-5 absolute left-3 top-1/2 -translate-y-1/2 text-gray-400" 
+            fill="none" 
+            viewBox="0 0 24 24" 
             stroke="currentColor"
           >
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
           </svg>
         </div>
-        <button type="submit" className="btn btn-primary">Search</button>
+        <button type="submit" className="btn btn-primary join-item">Search</button>
       </div>
     </form>
   );
